97fa64901c2d5a3eda3fbc03b0fade8d9efbd60e
oguzcan.sertel <oguzcan.sertel@modanisa.com>
2021-03-02 16:53:13 +0300

    rev

--- channels/channel.go
+++ channels/channel.go
@@ -3,6 +3,8 @@ package main
 import "fmt"
 
 func main() {
+	// We just a create fibonacci function which is shows nth fibonacci value
+	// The problem how to show fibonacci series with goroutines and channels
 	fmt.Println(fib(10))
 }
 
