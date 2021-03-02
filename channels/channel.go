db82d5dbf1df3b905a8ab58d594294425f5c5fe9
oguzcan.sertel <oguzcan.sertel@modanisa.com>
2021-03-02 03:59:43 +0300

    step12

--- channels/channel.go
+++ channels/channel.go
@@ -2,19 +2,17 @@ package main
 
 import (
 	"fmt"
-	"time"
 )
 
 func main() {
-	ch := make(chan string)
-	// We created another goroutine and send value to channel in that gourutine.
-	// So the execution will not stop
-	go anotherRoutine(ch)
-	msg := <-ch // this will block the execution until it receives the value from channel
-	fmt.Println(msg)
-}
+	// When we use size parameter for chan type this will create new goroutines.
+	ch := make(chan string, 2)
 
-func anotherRoutine(ch chan string) {
-	ch <- "hello"
-	time.Sleep(time.Second * 10) // this will never fire because sending value to channel will block the execution
+	ch <- "hello" // This will not block the execution because we gave 2 gourutines in make for chan type
+	msg := <-ch
+	fmt.Println(msg)
+
+	ch <- "world"
+	msg = <-ch
+	fmt.Println(msg)
 }
