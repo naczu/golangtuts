e385277731352684a4ca8d5c4fb584a0da9b260d
oguzcan.sertel <oguzcan.sertel@modanisa.com>
2021-03-02 03:51:41 +0300

    step10

--- channels/channel.go
+++ channels/channel.go
@@ -1,15 +1,20 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"time"
+)
 
 func main() {
 	ch := make(chan string)
-	// Here we blocked the execution in same goroutine. So this will not work.
-	// We cannot send value to channels in same goroutine.
-	fmt.Println("Now execution will be blocked")
-	ch <- "hello" // this blocks the execution
-	fmt.Println("This will never fire")
-	msg := <-ch
+	// We created another goroutine and send value to channel in that gourutine.
+	// So the execution will not stop
+	go anotherRoutine(ch)
+	msg := <-ch // this will block the execution until it receives the value from channel
 	fmt.Println(msg)
+}
 
+func anotherRoutine(ch chan string) {
+	time.Sleep(time.Second * 1)
+	ch <- "hello"
 }
